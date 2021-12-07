3b997b9d43dc97a40610145223cfb2bcbaac21f4
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-07 16:57:54 +0100

    Task 8 done with js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-
+//-------Exercises 7---------//
 let toChange = document.getElementById("tochange")
 toChange.innerHTML = "Something"
 
 
 
-console.log(toChange);
+//-------Exercises 8---------//
+
+let newBackgroundColor = document.querySelector("body")
+
+newBackgroundColor.style.background = "grey"
+
+
+console.log(newBackgroundColor);
 
 
