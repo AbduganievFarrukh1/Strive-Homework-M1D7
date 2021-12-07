0720072f4dab2620aadaf412e1bf615cc09aba98
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-07 17:48:26 +0100

    Task 10 Done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-//-------Exercises 7---------//
-let toChange = document.getElementById("tochange")
-toChange.innerHTML = "Something"
+// //-------Exercises 7---------//
+// let toChange = document.getElementById("tochange")
+// toChange.innerHTML = "Something"
 
 
 
-//-------Exercises 8---------//
+// //-------Exercises 8---------//
 
-let newBackgroundColor = document.querySelector("body")
+// let newBackgroundColor = document.querySelector("body")
 
-newBackgroundColor.style.background = "grey"
+// newBackgroundColor.style.background = "grey"
 
 
-console.log(newBackgroundColor);
+// console.log(newBackgroundColor);
 
 
